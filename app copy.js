c6589e0ec576c12486535c84eccd07d647afad94
DenisMalv <kissed03@gmail.com>
2022-06-18 17:18:08 +0300

    com1
    работали с консолью, командами.
    разобрали path, fs.
    были примеры с реекспортом, и ООП

---
+++ app copy.js
@@ -0,0 +1,40 @@
+// console.log("NODE_ENV", process.env.NODE_ENV);
+// // console.log(process.env.jeyn);
+// console.log("ARGV", process.argv);
+// const { denis, jeyn } = require("./reexport");
+// console.log(denis.drink);
+// console.log(denis.dish);
+// console.log(jeyn.drink);
+// console.log(jeyn.dish);
+
+const { Calculator } = require("./calculatorOOP");
+
+// class Calculator {
+//   static PI = 3.14;
+//   static sum = (a, b) => {
+//     console.log(this);
+//     return a + b;
+//   };
+// }
+
+// console.log(Calculator.PI);
+// console.log(Calculator.sum(2, 5));
+
+// class User {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   sayHelo() {
+//     console.log("this", this);
+//     console.log(this.name);
+//   }
+// }
+
+// new User("den").sayHelo();
+// new User("jeyn").sayHelo();
+
+// // const den = new User("den");
+// // const jeyn = new User("jeyn");
+
+// // den.sayHelo();
+// // jeyn.sayHelo();
--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-// console.log("NODE_ENV", process.env.NODE_ENV);
-// // console.log(process.env.jeyn);
-// console.log("ARGV", process.argv);
-const { denis, jeyn } = require("./reexport");
-console.log(denis.drink);
-console.log(denis.dish);
-console.log(jeyn.drink);
-console.log(jeyn.dish);
+const path = require("path");
+require("./files");
+// // console.log(path);
+// console.log(__dirname);
+// console.log(__filename);
